04969a8b7896300044b5684ca0fcc47c176acb7c
Kevin Shaw <shaw@g2ss.com>
2016-05-02 10:45:21 -0700

    Add test case for both pickers using the same model

--- js/index.js
+++ js/index.js
@@ -5,27 +5,28 @@
     "ngAria",
     "ngMessages",
     "mdPickers"
-  ]); 
-  
+  ]);
+
   module.controller("MainCtrl", ['$scope', '$mdpDatePicker', '$mdpTimePicker', function($scope, $mdpDatePicker, $mdpTimePicker){
   	this.showDatePicker = function(ev) {
     	$mdpDatePicker($scope.currentDate, {
         targetEvent: ev
       }).then(function(selectedDate) {
         $scope.currentDate = selectedDate;
-      });;
+      });
     };
-    
+
     this.filterDate = function(date) {
       return moment(date).date() % 2 == 0;
     };
-    
+
     this.showTimePicker = function(ev) {
     	$mdpTimePicker($scope.currentTime, {
         targetEvent: ev
       }).then(function(selectedDate) {
         $scope.currentTime = selectedDate;
-      });;
-    }  
+      });
+    }
+
   }]);
 })();
